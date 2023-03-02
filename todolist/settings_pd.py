8ad57da229796b3984b95eaae5a09448300f1d09
BityutskiyNA <BityutskiyNA@gmail.com>
2023-03-02 15:44:06 +0300

    "user_done25"

--- todolist/settings_pd.py
+++ todolist/settings_pd.py
@@ -5,17 +5,14 @@ class Settings_TDL(BaseSettings):
     POSTGRES_DB: str
     POSTGRES_USER : str
     POSTGRES_PASSWORD : str
-    # DB_HOST='postgres'
     DB_HOST : str
     DB_PORT : str
-    # DJ_SECRET_KEY : str
     DJ_DEBUG : bool
-    # DATABASE_URL : str
     DJANGO_ALLOWED_HOSTS : str
     SOCIAL_AUTH_VK_OAUTH2_KEY: str
     SOCIAL_AUTH_VK_OAUTH2_SECRET:str
 
-    class Config:
-        validate_all = False
-        validate_assignment = False
-    #      env_file = '../.env_local'
+    # class Config:
+    #     validate_all = False
+    #     validate_assignment = False
+    # #      env_file = '../.env_local'
