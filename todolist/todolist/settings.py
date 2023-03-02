bdcbb150f74e1354e232433f5e9ee725d5b23bc6
BityutskiyNA <BityutskiyNA@gmail.com>
2023-03-02 21:54:08 +0300

    "user_done32"

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -8,6 +8,9 @@ from envparse import env
 from settings_pd import Settings_TDL
 
 setings_bs = Settings_TDL()
+print(setings_bs)
+print(os.getenv("POSTGRES_DB"))
+print(os.getenv("DB_HOST"))
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-!_3=8%7!(m+%kxcyjj7$#f6=+%s(d!*s&35git1x7x&+hy&w+8'
