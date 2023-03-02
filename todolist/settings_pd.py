1032b8c806c5b7e26a6660e94ff55f9ced0b0c13
BityutskiyNA <BityutskiyNA@gmail.com>
2023-03-02 16:10:30 +0300

    "user_done27"

--- todolist/settings_pd.py
+++ todolist/settings_pd.py
@@ -1,16 +1,18 @@
+import os
+
 from pydantic import BaseSettings
 
 
 class Settings_TDL(BaseSettings):
-    POSTGRES_DB: str
-    POSTGRES_USER : str
-    POSTGRES_PASSWORD : str
-    DB_HOST : str
-    DB_PORT : str
-    DJ_DEBUG : bool
-    DJANGO_ALLOWED_HOSTS : str
-    SOCIAL_AUTH_VK_OAUTH2_KEY: str
-    SOCIAL_AUTH_VK_OAUTH2_SECRET:str
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD : str = os.getenv("POSTGRES_PASSWORD")
+    DB_HOST : str = os.getenv("DB_HOST")
+    DB_PORT : str = os.getenv("DB_PORT")
+    DJ_DEBUG : bool = os.getenv("DJ_DEBUG")
+    DJANGO_ALLOWED_HOSTS : str = os.getenv("DJANGO_ALLOWED_HOSTS")
+    SOCIAL_AUTH_VK_OAUTH2_KEY: str = os.getenv("SOCIAL_AUTH_VK_OAUTH2_KEY")
+    SOCIAL_AUTH_VK_OAUTH2_SECRET:str = os.getenv("SOCIAL_AUTH_VK_OAUTH2_SECRET")
 
     # class Config:
     #     validate_all = False
