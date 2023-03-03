e6a96f2e258fc46eb3516867abd350a3f904fe61
BityutskiyNA <BityutskiyNA@gmail.com>
2023-03-03 15:52:37 +0300

    "user_done32"

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from core import views
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 
 # from django.contrib.auth import views
 # path('profile', csrf_exempt(views.UserRetrieveUpdateDestroyView.as_view())),
@@ -10,7 +10,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('login', views.UserLogonView.as_view()),
-    path('profile',views.UserRetrieveUpdateDestroyView.as_view() ),
+    path('profile',ensure_csrf_cookie(views.UserRetrieveUpdateDestroyView.as_view() )),
     path('signup', views.UserCreateView.as_view()),
     path('update_password', views.PasswordUpdateAPIView.as_view()),
 ]
