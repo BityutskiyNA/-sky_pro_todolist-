6241a685f781d4fbbd16d1aabf71075df888f645
BityutskiyNA <BityutskiyNA@gmail.com>
2023-03-03 21:21:39 +0300

    "done"

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -4,20 +4,15 @@ from django.contrib.auth.models import Group
 
 from core.models import User
 
-# Register your models here.
-# admin.site.register(User)
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    # inlines = (GenreFilmworkInline, PersonFilmworkInline,)
-    # # Отображение полей в списке
     list_display = ('username', 'email', 'first_name','last_name')
 
     # Фильтрация в списке
     list_filter = ('is_staff','is_active', 'is_superuser',)
 
-    search_fields = ('emailfirst_name', 'last_name', 'username')
+    search_fields = ('email', 'first_name', 'last_name', 'username')
 
-    exclude = ['password']
     readonly_fields = ['last_login', 'date_joined',]
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
