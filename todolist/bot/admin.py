69290c6050913c4e489b91d1ebc87d33de431efa
BityutskiyNA <BityutskiyNA@gmail.com>
2023-03-08 15:13:36 +0300

    "bot_done_5"

--- todolist/bot/admin.py
+++ todolist/bot/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from bot.models import TgUser
 
-admin.register(TgUser)
-
-
 @admin.register(TgUser)
 class TgUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'user')
